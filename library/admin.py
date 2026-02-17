cbe8b28b0c14bee7addcc8dc41220d61aacfb934
Adeniyi David <bemintdav@gmail.com>
2026-02-17 20:54:14 +0100

    Add academic state management to admin panel with advance and rollback actions

--- library/admin.py
+++ library/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import *
+from .models import AcademicState
+from .services import advance_academic_state, rollback_academic_state
 
 admin.site.register(School)
 admin.site.register(Department)
@@ -12,4 +14,21 @@ admin.site.register(Approval)
 admin.site.register(Rejection)
 admin.site.register(ActionLog)
 admin.site.register(AcademicState)
+
+@admin.register(AcademicState)
+class AcademicStateAdmin(admin.ModelAdmin):
+    list_display = ("current_semester",)
+    actions = ["advance", "rollback"]
+
+    def advance(self, request, queryset):
+        message = advance_academic_state()
+        self.message_user(request, message)
+
+    advance.short_description = "Advance academic state"
+
+    def rollback(self, request, queryset):
+        message = rollback_academic_state()
+        self.message_user(request, message)
+
+    rollback.short_description = "Rollback academic state"
 
