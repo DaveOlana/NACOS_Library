ac8a7ce3d70b070aa76db7f842b643305525bc07
Adeniyi David <bemintdav@gmail.com>
2026-02-17 21:21:22 +0100

    Refactor Material model structure and fix duplicate model registrations

--- library/admin.py
+++ library/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import *
-from .models import AcademicState
+from .models import (School, Department, Level, Semester, StudentProfile, 
+                     AcademicState, Course, Material, MaterialUpload)
 from .services import advance_academic_state, rollback_academic_state
 
 admin.site.register(School)
@@ -8,12 +8,6 @@ admin.site.register(Department)
 admin.site.register(Level)
 admin.site.register(Semester)
 admin.site.register(StudentProfile)
-admin.site.register(Material)
-admin.site.register(MaterialPage)
-admin.site.register(Approval)
-admin.site.register(Rejection)
-admin.site.register(ActionLog)
-admin.site.register(AcademicState)
 
 @admin.register(AcademicState)
 class AcademicStateAdmin(admin.ModelAdmin):
@@ -31,4 +25,8 @@ class AcademicStateAdmin(admin.ModelAdmin):
         self.message_user(request, message)
 
     rollback.short_description = "Rollback academic state"
+
+admin.site.register(Course)
+admin.site.register(Material)
+admin.site.register(MaterialUpload)
 
